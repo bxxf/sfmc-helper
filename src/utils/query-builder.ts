47d11795b6811f26e6d59baa08256a7e47a3052d
Filip Brebera <breberafilip@icloud.com>
2023-03-25 14:33:11 +0100

    fix: dependency

--- src/utils/query-builder.ts
+++ src/utils/query-builder.ts
@@ -1,6 +1,6 @@
 import type { ApiResponse, ComparisonOperator } from "sfmc-helper";
 
-import fetch from "cross-fetch";
+import fetch from "node-fetch";
 import { Filter } from "./filter";
 import { SfmcHelper } from "../sfmc-helper";
 
